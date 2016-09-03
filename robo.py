9ff6fee3f64a35a32d8f9b1e0af24b36e216bcfa
dany <danyzolotarev@gmail.com>
2016-09-03 21:18:07 +0300

    My new bot

--- robo.py
+++ robo.py
@@ -5,11 +5,9 @@
 # This program is dedicated to the public domain under the CC0 license.
 """
 This Bot uses the Updater class to handle the bot.
-
 First, a few handler functions are defined. Then, those functions are passed to
 the Dispatcher and registered at their respective places.
 Then, the bot is started and runs until we press Ctrl-C on the command line.
-
 Usage:
 Basic Echobot example, repeats messages.
 Press Ctrl-C on the command line or send a signal to the process to stop the
@@ -206,7 +204,6 @@ if __name__ == '__main__':
 
     cm = open("data/classmates.txt", "r")
     mates = [s[:-1] for s in cm.readlines()]
-
     commands = open("data/help_command.txt", "r")
     cmd = commands.readlines()
     commands.close()
